e544d519595ec098bd4d1596141306480bcfdbdd
Julio Silva <103950621+JuCanavans@users.noreply.github.com>
2024-03-04 11:57:14 -0300

    Atualização de modelo dark_mode nos buttons

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const alterarModoTela = document.querySelector('#alternar-modo-page');
 const bodyClear = document.querySelector('body');
 const formularioClear = document.querySelector('.fomulario');
-const linksClear = document.querySelector('.apresentacao-link');
+const linksClear = document.querySelectorAll('.apresentacao-link');
 const foto_clear = document.querySelector('.foto-principal');
 
 alterarModoTela.addEventListener('click', () =>{
     bodyClear.classList.toggle('clear_mode');
     formularioClear.classList.toggle('formulario_clear_mode');
     foto_clear.classList.toggle('foto-principal_clear_mode');
-    linksClear.classList.toggle('apresentacao-link_clear_mode');
+    linksClear.forEach((link) =>{/*Selecionado todos os button e realizando a alteração*/
+        link.classList.toggle('apresentacao-link_clear_mode');
+    })
 })
